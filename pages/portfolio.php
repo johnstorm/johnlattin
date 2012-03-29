5e73b12a3068b23d4a79879a190a30ee521f216b
John C. Lattin <xonnex@gmail.com>
2012-03-29 11:16:16 -0400

    Worked partially on the portfolio page, added pixelwave and appfigures.

--- pages/portfolio.php
+++ pages/portfolio.php
@@ -5,20 +5,42 @@
 			<h3>For Work</h3>
 			<ul class="items">
 				<li>
-					<a title="Lorem ipsum"><img src="images/thumb3.jpg" alt="" /></a>
+					<a title="Pixelwave"><img src="images/pixelwave.png" alt="" style="width:56px; height:56px;"/></a>
 					<div class="project_details">
-						<img src="images/project.gif" alt="" />
+						<img src="images/pixelwave.png" alt="" style="width:263px; height:174px;"/>
 						<div class="description">
-							<h3>Lorem ipsum</h3>
+							<h3>Pixelwave</h3>
 							<p>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam porta, nisi a ornare scelerisque, nibh tortor pretium tortor, sed tempus est ipsum sed arcu.
+								Lead Programmer<br>
+								Pixelwave is an iOS framework built from scratch. It follows the AS3 API to make building 
+								games, and other applications, easy
 							</p>
 							<ul class="default">
- 								<li>CSS &amp; XHTML front end</li>
-								<li>Back end development</li>
-								<li>PSD design</li>
+ 								<li>C</li>
+								<li>Objective-C</li>
+								<li>OpenGL ES 1.1</li>
 							</ul>
-							<div class="links"><a href="#" target="_blank">Visit Site</a> <a class='to_portfolio'>&laquo; Back</a></div>
+							<div class="links"><a href="http://pixelwave.org/" target="_blank">Visit Site</a> <a class='to_portfolio'>&laquo; Back</a></div>
+						</div>
+					</div>
+				</li>
+				<li>
+					<a title="appFigures"><img src="images/appfigures.png" alt="" style="width:56px; height:56px;"/></a>
+					<div class="project_details">
+						<img src="images/appfigures.png" alt="" style="width:263px; height:174px;"/>
+						<div class="description">
+							<h3>appFigures</h3>
+							<p>
+								Front End Developer<br>
+								appFigures is a reporting platform for mobile app developers. It is designed with our users in 
+								mind to display to them, their important information, in the easiest readable format
+							</p>
+							<ul class="default">
+ 								<li>Javascript</li>
+								<li>HTML</li>
+								<li>CSS</li>
+							</ul>
+							<div class="links"><a href="http://www.appfigures.com/" target="_blank">Visit Site</a> <a class='to_portfolio'>&laquo; Back</a></div>
 						</div>
 					</div>
 				</li>
